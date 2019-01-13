38b90650ce4a56596014c17524e2adb90fb05af8
vhlaynez <35643243+vhlaynez@users.noreply.github.com>
2019-01-13 21:54:21 -0200

    Update conexao.php

--- tcc/conexao/conexao.php
+++ tcc/conexao/conexao.php
@@ -1,8 +1,8 @@
 <?php
-	$servidor = "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-	$usuario = "snam60v5qd60olbe";
-	$senha = "eo3tcicft75wtkyd";
-	$banco = "lhpf7436mnacfhpn";
+	$servidor = "";
+	$usuario = "";
+	$senha = "";
+	$banco = "";
 	$conecta = mysqli_connect($servidor, $usuario, $senha, $banco);
 
 	if ( mysqli_connect_errno() ) {
